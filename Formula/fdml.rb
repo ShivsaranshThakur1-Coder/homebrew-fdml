bbe925c780db37a8d9de7bf0928c6e9f9398bcb3
ShivsaranshThakur1-Coder <a.schalk.manchester@gmail.com>
2025-10-13 21:51:26 +0100

    fdml 0.3.0 (new formula)

---
+++ Formula/fdml.rb
@@ -0,0 +1,29 @@
+class Fdml < Formula
+  desc "Folk Dance Markup Language CLI"
+  homepage "https://shivsaranshthakur1-coder.github.io/fdml-core/"
+  url "https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.0/fdml-core.jar", using: :nounzip
+  version "0.3.0"
+  sha256 "PASTE_SHA256_HERE"
+
+  depends_on "openjdk@17"
+
+  def install
+    libexec.install "fdml-core.jar"
+    (bin/"fdml").write <<~EOS
+      #!/usr/bin/env bash
+      exec "#{Formula["openjdk@17"].opt_bin}/java" -jar "#{libexec}/fdml-core.jar" "$@"
+    EOS
+    chmod 0555, bin/"fdml"
+  end
+
+  test do
+    (testpath/"t.fdml.xml").write <<~XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <fdml version="1.0">
+        <meta><title>t</title></meta>
+        <body><section id="s-1">ok</section></body>
+      </fdml>
+    XML
+    system "#{bin}/fdml", "validate", testpath/"t.fdml.xml"
+  end
+end

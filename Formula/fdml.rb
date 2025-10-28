0d471ea8286a39d2ba4388ee2b96c8c63f1069bf
ShivsaranshThakur1-Coder <a.schalk.manchester@gmail.com>
2025-10-28 11:04:55 +0000

    fdml v0.3.4: fix wrapper interpolation; set url=https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar sha256=477c6d08f41798228abb6e1414d83e4e7d67392b5319ccd7371a1fe558932ff8

--- Formula/fdml.rb
+++ Formula/fdml.rb
@@ -1,17 +1,17 @@
 class Fdml < Formula
   desc "Folk Dance Markup Language CLI"
   homepage "https://shivsaranshthakur1-coder.github.io/fdml-core/"
-  url "https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar", using: :nounzip
-  version "0.3.4"
-  sha256 "477c6d08f41798228abb6e1414d83e4e7d67392b5319ccd7371a1fe558932ff8"
+  url "https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jarURLhttps://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar", using: :nounzip
+  version "https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jarVERhttps://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar"
+  sha256 "https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jarSHAhttps://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar"
 
-  depends_on "openjdk@17"
+  depends_on "openjdkhttps://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar17"
 
   def install
     libexec.install "fdml-core.jar"
     (bin/"fdml").write <<~EOS
       #!/usr/bin/env bash
-      exec "\#{Formula["openjdk@17"].opt_bin}/java" -jar "\#{libexec}/fdml-core.jar" ""
+      exec "#{Formula["openjdkhttps://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar17"].opt_bin}/java" -jar "#{libexec}/fdml-core.jar" "$https://github.com/ShivsaranshThakur1-Coder/fdml-core/releases/download/v0.3.4/fdml-core.jar"
     EOS
     chmod 0555, bin/"fdml"
   end
@@ -24,6 +24,6 @@ class Fdml < Formula
         <body><section id="s-1">ok</section></body>
       </fdml>
     XML
-    system "\#{bin}/fdml", "validate", testpath/"t.fdml.xml"
+    system "#{bin}/fdml", "validate", testpath/"t.fdml.xml"
   end
 end
